bd111b40026837710fdac4165a651823ab4bc843
Andy Bulka <abulka@gmail.com>
2020-10-17 10:27:51 +1100

    bump

--- relmgr/__init__.py
+++ relmgr/__init__.py
@@ -4,6 +4,6 @@ from relmgr.relationship_manager import RelationshipManager
 from relmgr.relationship_manager import RelationshipManager as RelMgr  # shorter name
 
 # Version of relationship-manager package
-__version__ = "1.4.2"
+__version__ = "1.4.3"
 
 __all__ = ["RelationshipManager", "RelMgr"]  # for "from relmgr import *"" syntax
